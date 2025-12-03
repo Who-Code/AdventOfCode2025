3ac41117ddc9ef5f5371a3414f7a8d0986d9ea1b
WhoCode <robertkeller04@gmail.com>
2025-12-03 08:05:21 +0100

    Prepared part 2

--- Solutions/day_3_joltage_solver.py
+++ Solutions/day_3_joltage_solver.py
@@ -14,11 +14,25 @@ def highest_joltage(joltage_line):
     for idx, joltage in enumerate(joltage_list):
         sub_joltage_list = joltage_list[idx+1:len(joltage_list)]
         for joltage_second in sub_joltage_list:
-            current_value = int(joltage+joltage_second);
+            current_value = int(joltage+joltage_second)
             if current_value > current_highest:
                 current_highest = current_value
     return current_highest
 
+def highest_joltage_by_12(joltage_line):
+    # now we need to find the highest joltage of numbers with 12 digits builded from the inout
+    current_highest = 0
+    joltage_list = list(joltage_line)
+    # loop trough the joltage items
+    for idx, joltage in enumerate(joltage_list):
+        current_sum = 0
+        for cx in range(1,11-idx):
+            sub_joltage_list = joltage_list[idx + 1 + cx:len(joltage_list)]
+            for joltage_second in sub_joltage_list:
+                current_sum += int(joltage + joltage_second);
+                if current_sum > current_highest:
+                    current_highest = current_sum
+    return current_highest
 
 def joltage_sum(joltage_lines):
     sum = 0
